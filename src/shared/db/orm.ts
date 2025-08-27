11ded041a8d07dfc954cf2bbc43523142f4ec90b
Martín Mercado <martinmercado1502@gmail.com>
2025-08-27 20:39:28 -0300

    Update orm.ts

--- src/shared/db/orm.ts
+++ src/shared/db/orm.ts
@@ -1,11 +1,10 @@
 import { MikroORM } from "@mikro-orm/core";
 import { SqlHighlighter } from "@mikro-orm/sql-highlighter";
 
-export const orm = await MikroORM.init({
+export const ormPromise = MikroORM.init({
   entities: ["./dist/shared/db/entities/**/*.js"],
   entitiesTs: ["./src/shared/db/entities/**/*.ts"],
   dbName: "Kicket_db",
-  type: "mysql",
   clientUrl: "mysql://root:password@localhost:3306/kicket",
   highlighter: new SqlHighlighter(),
   debug: true,
@@ -18,6 +17,7 @@ export const orm = await MikroORM.init({
 });
 
 export const syncSchema = async () => {
+  const orm = await ormPromise;
   const generator = orm.getSchemaGenerator();
   /*
     await generator.dropSchema(); //never in production
